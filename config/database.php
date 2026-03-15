90a3bc96c6919357e27634a021dd3bd95f653950
Yasir-Ismail <yasirismail576@gmail.com>
2026-03-15 21:09:22 +0500

    Create database.php

---
+++ config/database.php
@@ -0,0 +1,14 @@
+<?php
+// config/database.php
+$host = 'localhost';
+$dbname = 'makkah_travels';
+$username = 'root';
+$password = '';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+    die("ERROR: Could not connect. " . $e->getMessage());
+}
+?>
